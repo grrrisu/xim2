0a70b47d45b29f322f3fa09815e07bd94328fa6c
Alessandro Di Maria <alessandro.dimaria@swisscom.com>
2023-10-26 15:18:00 +0200

    grid wrapper

--- apps/xim2_web/lib/xim2_web/live/grid_live/show.ex
+++ apps/xim2_web/lib/xim2_web/live/grid_live/show.ex
@@ -17,32 +17,48 @@ defmodule Xim2Web.GridLive.Show do
   def render(assigns) do
     ~H"""
     <h1>Grid</h1>
-      <div class="overflow-x-auto border border-red-500 bg-red-100">
-        <div class="m-2" style="height: 50px">
-          <p>Some text before the grid</p>
-          <.button phx-click="update">Update</.button>
-        </div>
-        <.grid :let={{x, y, value}} grid={@grid} class="m-6" style="width: 200vmin; height: 100vmin">
-          <.cell x={x} y={y} value={value} class="bg-green-300" />
-        </.grid>
+    <div class="border border-red-500 bg-red-100">
+      <div class="m-2" style="height: 50px">
+        <p>Some text before the grid</p>
+        <.button phx-click="update">Update</.button>
       </div>
+      <.grid :let={{x, y, value}} grid={@grid} width="200vmin" height="100vmin" class="m-6">
+        <.cell x={x} y={y} value={value} class="bg-green-300" />
+      </.grid>
+    </div>
     """
   end
 
-
+  def grid(assigns) do
+    ~H"""
+    <div class="overflow-x-auto">
+      <.grid_container
+        :let={{x, y, value}}
+        grid={@grid}
+        class={@class}
+        style={"width: #{@width}; height: #{@height}"}
+      >
+        <%= render_slot(@inner_block, {x, y, Grid.get(@grid, x, y)}) %>
+      </.grid_container>
+    </div>
+    """
+  end
 
   # attr :type, :string, default: ""
-  def grid(assigns) do
+  def grid_container(assigns) do
     assigns = assign(assigns, width: Grid.width(assigns.grid), height: Grid.height(assigns.grid))
 
     ~H"""
     <div
       id="grid"
       class={["grid auto-rows-fr bg-gray-300 border-l border-b border-gray-900 h-full w-full", @class]}
-      style={["grid-template-columns: repeat(#{@width},1fr); grid-template-rows: repeat(#{@height},1fr);", @style]}
+      style={[
+        "grid-template-columns: repeat(#{@width},1fr); grid-template-rows: repeat(#{@height},1fr);",
+        @style
+      ]}
     >
-    <%= for y <- 0..(@height - 1) do %>
-      <%= for x <- 0..(@width - 1) do %>
+      <%= for y <- 0..(@height - 1) do %>
+        <%= for x <- 0..(@width - 1) do %>
           <%= render_slot(@inner_block, {x, y, Grid.get(@grid, x, y)}) %>
         <% end %>
       <% end %>
@@ -52,6 +68,7 @@ defmodule Xim2Web.GridLive.Show do
 
   def cell(assigns) do
     assigns = assign(assigns, id: "cell-#{assigns.x}-#{assigns.y}")
+
     ~H"""
     <div
       id={@id}
@@ -60,8 +77,8 @@ defmodule Xim2Web.GridLive.Show do
       phx-value-x={@x}
       phx-value-y={@y}
     >
-      <%=# "#{@x}:#{@y}" %>
-      <%=# @value %>
+      <%= # "#{@x}:#{@y}" %>
+      <%= # @value %>
     </div>
     """
   end
