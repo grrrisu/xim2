f4997cd1582485eb3644952ff99bee10764937c2
Alessandro Di Maria <alessandro.dimaria@swisscom.com>
2023-10-31 06:53:42 +0100

    create sub app biotope

---
+++ apps/biotope/lib/biotope.ex
@@ -0,0 +1,18 @@
+defmodule Biotope do
+  @moduledoc """
+  Documentation for `Biotope`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Biotope.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/biotope/lib/biotope/application.ex
@@ -0,0 +1,20 @@
+defmodule Biotope.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Biotope.Worker.start_link(arg)
+      # {Biotope.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Biotope.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
