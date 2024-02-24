1a92bb1c897dc9a8f5bcbdbc53c9a32730f3695e
Alessandro Di Maria <adm@m42.ch>
2024-02-24 11:20:22 +0100

    sub app monitor

---
+++ apps/monitor/lib/sim/monitor.ex
@@ -0,0 +1,18 @@
+defmodule Sim.Monitor do
+  @moduledoc """
+  Documentation for `Sim.Monitor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sim.Monitor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
