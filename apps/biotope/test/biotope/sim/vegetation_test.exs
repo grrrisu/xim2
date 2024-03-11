a75941a69d671b16e128698a8c80520c327b84a4
Alessandro Di Maria <adm@m42.ch>
2024-03-11 14:41:29 +0100

    return no_change if vegetation doesn’t grow anymore

--- apps/biotope/test/biotope/sim/vegetation_test.exs
+++ apps/biotope/test/biotope/sim/vegetation_test.exs
@@ -6,13 +6,13 @@ defmodule Biotope.Sim.VegetationTest do
   alias Biotope.Sim.Vegetation
 
   test "grow vegetation" do
-    %Vegetation{size: size} = Vegetation.grow(%Vegetation{size: 650})
+    %{size: size} = Vegetation.grow(%Vegetation{size: 650})
     assert 650 < size
   end
 
   test "sim" do
     data = start_supervised!(AccessData)
-    {:ok, biotope} = Data.create(1, 1, data)
+    {:ok, _biotope} = Data.create(1, 1, data)
     assert {{0, 0}, %{size: size}} = Vegetation.sim({0, 0}, data: data)
     assert 650 < size
   end
