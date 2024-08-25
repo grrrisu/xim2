b699007d2fe1c29ad19ae0a40ecb38ad1537a586
Alessandro Di Maria <adm@m42.ch>
2024-08-25 12:08:45 +0200

    use main_section

--- apps/xim2_web/lib/xim2_web/live/astrorunner_live/index.ex
+++ apps/xim2_web/lib/xim2_web/live/astrorunner_live/index.ex
@@ -1,13 +1,15 @@
 defmodule Xim2Web.AstrorunnerLive.Index do
   use Xim2Web, :live_view
 
+  import Xim2Web.ProjectComponents
+
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <.main_title>Astrorunner</.main_title>
+    <.main_section title="Astrorunner" back={~p"/"}></.main_section>
     """
   end
 end
