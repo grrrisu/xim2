bd53b4314af7ec3279accca7815d9854cd5b704f
Alessandro Di Maria <adm@m42.ch>
2024-08-26 22:48:45 +0200

    for each card

--- apps/xim2_web/lib/xim2_web/live/astrorunner_live/index.ex
+++ apps/xim2_web/lib/xim2_web/live/astrorunner_live/index.ex
@@ -5,23 +5,30 @@ defmodule Xim2Web.AstrorunnerLive.Index do
   import Monsum.BoardgameComponents
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, global_board: Astrorunner.global_board())}
   end
 
   def render(assigns) do
     ~H"""
     <.main_section title="Astrorunner" back={~p"/"}>
-      <.card>
-        <:title>Card Title</:title>
-        <:picture>
-          <.picture small={~p"/images/mountain-searching.avif"} />
-        </:picture>
-        <:body_title>Title</:body_title>
-        <:body>
-          Card Body
-        </:body>
-      </.card>
+      <h2>Arbeitsmarkt</h2>
+      <.cards deck={@global_board.cards.level_1} />
     </.main_section>
+    """
+  end
+
+  def cards(assigns) do
+    ~H"""
+    <.card :for={card <- @deck}>
+      <:title><%= card.title %></:title>
+      <:picture>
+        <.picture small={~p"/images/mountain-searching.avif"} />
+      </:picture>
+      <:body_title>Title</:body_title>
+      <:body>
+        <%= card.text %>
+      </:body>
+    </.card>
     """
   end
 end
