efc7fa1af3712c6a91405887e7083c01ab95df34
Alessandro Di Maria <adm@m42.ch>
2024-08-26 22:48:45 +0200

    for each card

--- apps/astrorunner/lib/astrorunner.ex
+++ apps/astrorunner/lib/astrorunner.ex
@@ -2,4 +2,10 @@ defmodule Astrorunner do
   @moduledoc """
   Digital version of the boardgame *Astrorunner*
   """
+
+  alias Astrorunner.Board
+
+  def global_board() do
+    Board.global_board()
+  end
 end
