81d66cfc79d3a3b542c1697fa2523031c08a8138
Alessandro Di Maria <adm@m42.ch>
2024-08-28 16:11:45 +0200

    board setup

--- apps/astrorunner/lib/astrorunner.ex
+++ apps/astrorunner/lib/astrorunner.ex
@@ -5,7 +5,23 @@ defmodule Astrorunner do
 
   alias Astrorunner.Board
 
+  def setup() do
+    Board.global_setup()
+  end
+
   def global_board() do
     Board.global_board()
+  end
+
+  def get_global_decks() do
+    Board.get_global(&fun_get_global_decks(&1))
+  end
+
+  def fun_get_global_decks(global) do
+    global
+    |> Map.get(:cards)
+    |> Enum.reduce(%{}, fn {key, value}, res ->
+      Map.put_new(res, key, value.revealed)
+    end)
   end
 end
--- apps/astrorunner/test/astrorunner_test.exs
+++ apps/astrorunner/test/astrorunner_test.exs
@@ -1,4 +1,23 @@
 defmodule AstrorunnerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Astrorunner
+
+  alias Astrorunner.{Card, Board, Deck}
+
+  setup do
+    %{board: Board.handle_global_setup()}
+  end
+
+  test "setup", %{board: board} do
+    assert %{cards: %{pilots: pilots, level_1: _level_1}} = board
+    assert %Deck{} = pilots
+    assert %Card{} = pilots.draw_pile |> List.first()
+  end
+
+  test "get global cards", %{board: board} do
+    %{pilots: pilots, level_1: level_1} = Astrorunner.fun_get_global_decks(board)
+    assert 4 == Enum.count(pilots)
+    assert 4 == Enum.count(level_1)
+    assert %Card{} = List.first(pilots)
+  end
 end
