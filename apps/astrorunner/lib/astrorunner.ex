1f3979b0c378a6a69dad903b1182cd9f47917fa1
Alessandro Di Maria <adm@m42.ch>
2024-08-28 23:10:14 +0200

    take card from job market

--- apps/astrorunner/lib/astrorunner.ex
+++ apps/astrorunner/lib/astrorunner.ex
@@ -22,11 +22,19 @@ defmodule Astrorunner do
     Board.get_global(&fun_get_global_decks(&1))
   end
 
+  def take_revealed_card(name: name, index: index, user: user) do
+    Board.update(&fun_take_revealed_card(&1, &2), name: name, index: index, user: user)
+  end
+
   def fun_get_global_decks(global) do
     global
     |> Map.get(:cards)
     |> Enum.reduce(%{}, fn {key, value}, res ->
       Map.put_new(res, key, value.revealed)
     end)
+  end
+
+  def fun_take_revealed_card(global, [name: name, index: index, user: user] = params) do
+    dbg(params)
   end
 end
