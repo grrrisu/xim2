b4bba5c03ef15019495b679dbce667008ad643ce
Alessandro Di Maria <adm@m42.ch>
2024-10-30 11:21:27 +0100

    food form

--- apps/xim2_web/lib/xim2_web/live/my_liege_live/index.ex
+++ apps/xim2_web/lib/xim2_web/live/my_liege_live/index.ex
@@ -5,28 +5,45 @@ defmodule Xim2Web.MyLiegeLive.Index do
   import Xim2Web.ProjectComponents
 
   def mount(_params, _session, socket) do
-    socket =
-      if connected?(socket) do
-        PubSub.subscribe(Xim2.PubSub, "my_liege")
-        MyLiege.create()
-        socket = assign(socket, realm: MyLiege.get_realm())
-      else
-        socket = assign(socket, realm: nil)
-      end
-
+    socket = if connected?(socket), do: prepare_realm(socket), else: assign(socket, realm: nil)
     {:ok, socket}
+  end
+
+  def prepare_realm(socket) do
+    PubSub.subscribe(Xim2.PubSub, "my_liege")
+    MyLiege.create()
+    socket = assign(socket, realm: MyLiege.get_realm())
+    form_fields = %{"food" => 0}
+    assign(socket, form: to_form(form_fields))
+  end
+
+  def handle_event("change_food", %{"realm" => %{"food" => food}}, socket) do
+    food = String.to_integer(food)
+    {:noreply, socket}
   end
 
   def render(assigns) do
     ~H"""
     <.main_section title="My Liege" back={~p"/"}>
       <%= if @realm do %>
+        <.food_form form={@form} />
         <.social_stratum social={@realm.working} name="Working" />
         <.social_stratum social={@realm.poverty} name="Poverty" />
       <% else %>
         <p>Loading...</p>
       <% end %>
     </.main_section>
+    """
+  end
+
+  def food_form(assigns) do
+    ~H"""
+    <.action_box class="mb-2">
+      <.form :let={form} for={@form} as={:realm} phx-submit="change_food" class="flex items-end">
+        <.input field={form[:food]} label="Food" />
+        <.button class="ml-2 h-10">Change</.button>
+      </.form>
+    </.action_box>
     """
   end
 
