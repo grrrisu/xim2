8f6ca2db5183305b944f791d9d1364a943da91f8
Alessandro Di Maria <adm@m42.ch>
2026-02-02 08:36:00 +0100

    bump versions

--- apps/biotope/mix.exs
+++ apps/biotope/mix.exs
@@ -4,7 +4,7 @@ defmodule Biotope.MixProject do
   def project do
     [
       app: :biotope,
-      version: "0.1.0",
+      version: "0.1.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
