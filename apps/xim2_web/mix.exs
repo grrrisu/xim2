9edd6b8c35b6c56282490847996827f73e06bc01
Alessandro Di Maria <adm@m42.ch>
2026-02-02 08:36:00 +0100

    bump versions

--- apps/xim2_web/mix.exs
+++ apps/xim2_web/mix.exs
@@ -4,7 +4,7 @@ defmodule Xim2Web.MixProject do
   def project do
     [
       app: :xim2_web,
-      version: "0.1.1",
+      version: "0.1.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
